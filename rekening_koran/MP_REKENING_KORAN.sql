1a591bae993b89b24ea097ae0785f723b83d21e4
justputoff <kholid.hasibuan75@gmail.com>
2025-10-06 13:11:15 +0700

    Update MP_REKENING_KORAN.sql

--- rekening_koran/MP_REKENING_KORAN.sql
+++ rekening_koran/MP_REKENING_KORAN.sql
@@ -1,10 +1,28 @@
-CREATE TABLE [MP_REKENING_KORAN] (
-    [id] BIGINT IDENTITY(1,1) PRIMARY KEY,
-    [trx_date] DATE NOT NULL,
-    [created_at] DATETIME NOT NULL,
-    [updated_at] DATETIME NOT NULL,
-    [credit] DECIMAL(18,2) NOT NULL,
-    [btp] VARCHAR(255) NOT NULL,
-    [keterangan] VARCHAR(255) NOT NULL,
-);
+USE [POWERAPPS]
+GO
+ 
+/****** Object:  Table [dbo].[MP_REKENING_KORAN]    Script Date: 06/10/2025 13:07:00 ******/
+SET ANSI_NULLS ON
+GO
+ 
+SET QUOTED_IDENTIFIER ON
+GO
+ 
+CREATE TABLE [dbo].[MP_REKENING_KORAN](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[trx_date] [date] NULL,
+	[created_at] [datetime] NULL,
+	[updated_at] [datetime] NULL,
+	[credit] [varchar](255) NULL,
+	[btp] [varchar](255) NULL,
+	[desc] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ 
+ 
+ 
  
