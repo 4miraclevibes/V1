39bd97efb398ab1da0de6556883b8da84c3f1e2c
justputoff <kholid.hasibuan75@gmail.com>
2025-10-22 19:43:11 +0700

    Update SP_MASTER_FindBTP_Batch.sql

--- rekening_koran/stored_procedures/MASTER/SP_MASTER_FindBTP_Batch.sql
+++ rekening_koran/stored_procedures/MASTER/SP_MASTER_FindBTP_Batch.sql
@@ -173,7 +173,7 @@ BEGIN
         );
         
         CREATE TABLE #TRSF_Results (
-            TransactionID NVARCHAR(50), TransactionDate NVARCHAR(50), Description NVARCHAR(500), CustomerName NVARCHAR(200),
+            TransactionID INT, TransactionDate NVARCHAR(50), Description NVARCHAR(MAX), CustomerName NVARCHAR(200),
             BTP NVARCHAR(50), MatchPercentage DECIMAL(5,2), MatchCount INT,
             TotalTransactions INT, LastLineNumber INT, TotalBTPOptions INT,
             OptionNumber INT, BestFlag NVARCHAR(10), LatestFlag NVARCHAR(10),
@@ -212,7 +212,7 @@ BEGIN
         );
         
         CREATE TABLE #BIFAST_Results (
-            TransactionID NVARCHAR(50), TransactionDate NVARCHAR(50), Description NVARCHAR(500), CustomerName NVARCHAR(200),
+            TransactionID INT, TransactionDate NVARCHAR(50), Description NVARCHAR(MAX), CustomerName NVARCHAR(200),
             BTP NVARCHAR(50), MatchPercentage DECIMAL(5,2), MatchCount INT,
             TotalTransactions INT, LastLineNumber INT, TotalBTPOptions INT,
             OptionNumber INT, BestFlag NVARCHAR(10), LatestFlag NVARCHAR(10),
@@ -251,7 +251,7 @@ BEGIN
         );
         
         CREATE TABLE #MANDIRI_Results (
-            TransactionID NVARCHAR(50), TransactionDate NVARCHAR(50), Description NVARCHAR(500), CustomerName NVARCHAR(200),
+            TransactionID INT, TransactionDate NVARCHAR(50), Description NVARCHAR(MAX), CustomerName NVARCHAR(200),
             BTP NVARCHAR(50), MatchPercentage DECIMAL(5,2), MatchCount INT,
             TotalTransactions INT, LastLineNumber INT, TotalBTPOptions INT,
             OptionNumber INT, BestFlag NVARCHAR(10), LatestFlag NVARCHAR(10),
