bb5ba000090d66d83f5b2d036eef7e98b9186563
justputoff <kholid.hasibuan75@gmail.com>
2025-10-31 09:36:23 +0700

    c
    c

--- distributors/update.sql
+++ distributors/update.sql
@@ -1,17 +1,10 @@
 -- Update distributor_id ke 10278 untuk customer dengan code yang ditentukan
 UPDATE [POWERAPPS].[dbo].[MP_CUSTOMER_NEW] 
-SET distributor_id = 10278
+SET distributor_id = 138
 WHERE code IN (
-    'COGJKS_000167',
-    'COGJKS_000205', 
-    'COGJKS_001210',
-    'COGJKS_001951',
-    'COGJKS_002002',
-    'COGJKS_002276',
-    'COGJKS_004066',
-    'COGJKS_004230',
-    'COGJKS_004348',
-    'COGJKS_004447',
-    'COGJKS_004565'
-)
+    'INUL-0008',
+    'KSGY-0008',
+    'INIF-0002',
+    'INUW-0057'
+);
 GO
